c282ed00c279f0426d3c4404d9514b1f1f74add2
jake23jad <jdudziak.soccer@gmail.com>
2024-04-07 00:06:53 -0500

    Displays Champion Name and Images (No styling)
    Deleted Champions Data json file (now all online apis). Now displays all champs and champ pictures.

--- src/app/Champions/page.tsx
+++ src/app/Champions/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useEffect, useState } from 'react';
-import Image from 'next/image';
+// import 
 
 export default function Layout() {
     // Define the Champion interface
@@ -19,15 +19,15 @@ export default function Layout() {
     }
 
     // Declare the state variable with its type
-    const [championId, setChampionId] = useState<string>("");
+    const [championIds, setChampionIds] = useState<string[]>([]);
 
     useEffect(() => {
         // Fetch data when component mounts
         fetch('https://ddragon.leagueoflegends.com/cdn/14.7.1/data/en_US/champion.json') // Assuming you have an API endpoint serving champion data
             .then(response => response.json())
             .then((data: ChampionInfo) => {
-                const championData = Object.values(data.data);
-                setChampionId(championData[0].id); // Set championId state
+                const championIds = Object.keys(data.data);
+                setChampionIds(championIds); // Set championIds state
             })
             .catch(error => console.error('Error fetching champion data:', error));
     }, []);
@@ -44,17 +44,21 @@ export default function Layout() {
                     <h1>Version: {/* Display version here */}</h1>
                     <div>
                         Info
-                        <div>
-                            {championId}
-                        </div>
-                        <div className="champion-face">
-                            <Image
-                                src={`https://static.bigbrain.gg/assets/lol/riot_static/14.6.1/img/champion/${championId}.png`}
-                                alt={`${championId} from League of Legends`}
-                                width={100}
-                                height={100}
-                                className='image-component'
-                            />
+                        <div className='champion-container'>
+                            {championIds.map(championId => (
+                                <div key={championId}>
+                                    {championId}
+                                    <div className="champion-face">
+                                        <img
+                                            src={`https://static.bigbrain.gg/assets/lol/riot_static/14.6.1/img/champion/${championId}.png`}
+                                            alt={`${championId} from League of Legends`}
+                                            width={100}
+                                            height={100}
+                                            className='image-component'
+                                        />
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
