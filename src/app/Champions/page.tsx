835746f30105a3bef3c5a6cb7710cce34360dcec
jake23jad <jdudziak.soccer@gmail.com>
2024-04-09 23:01:10 -0500

    Cleaned Up Code
    The champions file is cleaned up a bit. A image component was made to render the champion which can also be used on the summoner home page. Created a not-found to implement when you go to link champions/(not-a-champion-name).

--- src/app/Champions/page.tsx
+++ src/app/Champions/page.tsx
@@ -3,21 +3,20 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
-import '../../styles/champion.css'
+import ChampionImage from '../components/championImage';
+
+import '../../styles/champion.css';
 
 export default function Champions() {
     // Define the Champion interface
     interface Champion {
-        version: string;
         id: string;
         name: string;
-        // Add others if needed
     }
     
     // Define the ChampionInfo interface that is from data from api
     interface ChampionInfo {
-        type: string;
-        format: string;
+        
         version: string;
         data: Record<string, Champion>; // Represents an object with string keys and Champion values
     }
@@ -27,14 +26,18 @@ export default function Champions() {
     const [baseVersion, setBaseVersion] = useState<string>("");
 
     useEffect(() => {
-        // Fetch data when component mounts
+        // Fetch champion data from api 
         fetch('https://ddragon.leagueoflegends.com/cdn/14.7.1/data/en_US/champion.json') 
             .then(response => response.json())
             .then((data: ChampionInfo) => {
-                const champions = Object.values(data.data).map(champion => ({
-                    id: champion.id,
-                    name: champion.name
-                }));
+                const champions: { id: string; name: string }[] = [];
+                for (const championId in data.data) {
+                    const champion = data.data[championId];
+                    champions.push({
+                        id: champion.id,
+                        name: champion.name
+                    });
+                }
                 setChampionData(champions); // Set championData state
                 setBaseVersion(data.version); // Set baseVersion state
             })
@@ -43,48 +46,39 @@ export default function Champions() {
 
     return (
         <div>
-          <div className='header'>
-            <input className="search-input-box" type="text" />
-            <button className='search-button'>Search</button>
-          </div>
-          <div>
-            
-            <div className='all-champions'>
-              <div className='champion-header'>
-                <h1 className="champion-info-header">Champions</h1>
-                <div className='champion-header-mini'>
-                  <div>
-                    Click Champion for More Info 
-                  </div>
-                  <div>
-                    Patch: {baseVersion}
-                  </div>
-                </div>
-              </div>
-              <div className='champion-container'>
-                {championData.map(champion => (
-                  <Link key={champion.name} href={`/Champions/${champion.name}`}>
-                      
-                      <div className='champ-name-icon'>
-                        <div className='champ-name'>
-                          {champion.name}
-                        </div>
-                        <div className="champion-face">
-                          <img
-                            src={`https://static.bigbrain.gg/assets/lol/riot_static/14.7.1/img/champion/${champion.id}.png`}
-                            alt={`${champion.name} from League of Legends`}
-                            width={100}
-                            height={100}
-                            className='champion-image'
-                          />
-                        </div>
-                      </div>
-                    
-                  </Link>
-                ))}
-              </div>
+            <div className='header'>
+                <input className="search-input-box" type="text" />
+                <button className='search-button'>Search</button>
             </div>
-          </div>
+            <div>
+                <div className='all-champions'>
+                    <div className='champion-header'>
+                        <h1 className="champion-info-header">Champions</h1>
+                        <div className='champion-header-mini'>
+                            <div>
+                                Click Champion for More Info 
+                            </div>
+                            <div>
+                                Patch: {baseVersion}
+                            </div>
+                        </div>
+                    </div>
+                    <div className='champion-container'>
+                        {championData.map(champion => (
+                            <Link key={champion.name} href={`/Champions/${champion.name}`}>
+                                <div className='champ-name-icon'>
+                                    <div className='champ-name'>
+                                        {champion.name}
+                                    </div>
+                                    <div className="champion-face">
+                                        <ChampionImage champion={champion} />
+                                    </div>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
-      );
+    );
 }
