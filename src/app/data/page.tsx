4bcd934157df136c6c6d3c566c71071665470ad0
jake23jad <jdudziak.soccer@gmail.com>
2024-04-15 21:02:03 -0500

    Optimized Suboptimal Code
    The code was a bit beefy and unreadable. Cleaned it up a bit while also simplifying other areas.

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -2,10 +2,12 @@
 import { useEffect, useState } from "react";
 
 export default function Data() {
-    const [rankData, setRankData] = useState<any>(null);
-    const [accountData, setAccountData] = useState<any>(null);
-    const [matchIdData, setMatchIdData] = useState<any>(null);
-    const [matchData, setMatchData] = useState<any>(null);
+    const [data, setData] = useState({
+        rankData: null,
+        accountData: null,
+        matchIdData: null,
+        matchData: null
+    });
 
     const gameType = "RANKED_SOLO_5x5";
     const rank = "GOLD";
@@ -19,15 +21,16 @@ export default function Data() {
             try {
                 const response = await fetch(APICallString);
                 if (response.ok) {
-                    const data = await response.json();
-                    setRankData(data);
+                    const rankData = await response.json();
+                    setData(prevData => ({
+                        ...prevData,
+                        rankData
+                    }));
 
-                    // Limit the requests to one summoner
-                    if (data.length > 0) {
-                        // Select the first summoner from the data
-                        const selectedSummoner = data[2];
-                        // Fetch account data for the selected summoner
-                        await fetchMatchHistory(selectedSummoner.summonerId, API_KEY, setAccountData, setMatchIdData, setMatchData);
+                    // Process first summoner if available
+                    if (rankData.length > 0) {
+                        const selectedSummoner = rankData[2]; // You can choose another index if desired
+                        await fetchMatchHistory(selectedSummoner.summonerId, API_KEY);
                     }
                 } else {
                     console.error("API response error:", response.statusText);
@@ -39,129 +42,108 @@ export default function Data() {
         fetchData();
     }, []);
 
+    const fetchMatchHistory = async (summonerId: string, API_KEY: string) => {
+        const APIFetchAccount = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/${summonerId}?api_key=${API_KEY}`;
+
+        try {
+            const response = await fetch(APIFetchAccount);
+            if (response.ok) {
+                const accountData = await response.json();
+                setData(prevData => ({
+                    ...prevData,
+                    accountData
+                }));
+
+                const puuid = accountData.puuid;
+                await fetchMatchIds(puuid, API_KEY);
+            } else {
+                console.error("API response error:", response.statusText);
+            }
+        } catch (error) {
+            console.error("Fetch error:", error);
+        }
+    };
+
+    const fetchMatchIds = async (puuid: string, API_KEY: string) => {
+        if (!puuid || !API_KEY) {
+            console.error("Invalid inputs: PUUID or API_KEY is missing.");
+            return;
+        }
+
+        const APIFetchMatchId = `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=${API_KEY}`;
+
+        try {
+            const response = await fetch(APIFetchMatchId);
+            if (response.ok) {
+                const matchIds = await response.json();
+                setData(prevData => ({
+                    ...prevData,
+                    matchIdData: matchIds
+                }));
+
+                const matchOne = matchIds[0];
+                await fetchMatchData(matchOne, API_KEY);
+            } else {
+                console.error("API response error:", response.status, response.statusText);
+            }
+        } catch (error) {
+            console.error("Fetch error:", error);
+        }
+    };
+
+    const fetchMatchData = async (matchOne: string, API_KEY: string) => {
+        if (!matchOne || !API_KEY) {
+            console.error("Invalid inputs: Match ID or API_KEY is missing.");
+            return;
+        }
+
+        const FetchMatchData = `https://americas.api.riotgames.com/lol/match/v5/matches/${matchOne}?api_key=${API_KEY}`;
+
+        try {
+            const response = await fetch(FetchMatchData);
+            if (response.ok) {
+                const matchData = await response.json();
+                setData(prevData => ({
+                    ...prevData,
+                    matchData
+                }));
+            } else {
+                console.error("API response error:", response.status, response.statusText);
+            }
+        } catch (error) {
+            console.error("Fetch error:", error);
+        }
+    };
+
     return (
         <>
             <div>Get Data from Riot API</div>
             <div>
-                {rankData &&
+                {/* {data.rankData && (
                     <div>
-                        <h3>Account Data</h3>
-                        <pre>{JSON.stringify(accountData, null, 2)}</pre>
+                        <h3>Rank Data</h3>
+                        <pre>{JSON.stringify(data.rankData, null, 2)}</pre>
                     </div>
-                }
-                {accountData && (
+                )} */}
+                {data.accountData && (
                     <div>
                         <h3>Account Data</h3>
-                        <pre>{JSON.stringify(accountData, null, 2)}</pre>
+                        <pre>{JSON.stringify(data.accountData, null, 2)}</pre>
                     </div>
                 )}
-                {matchIdData && (
+                {data.matchIdData && (
                     <div>
                         <h3>Match ID Data</h3>
-                        <pre>{JSON.stringify(matchIdData, null, 2)}</pre>
+                        <pre>{JSON.stringify(data.matchIdData, null, 2)}</pre>
                     </div>
                 )}
-                {matchData && (
+                {data.matchData && (
                     <div>
                         <h3>Match Data</h3>
-                        <pre>{JSON.stringify(matchData, null, 2)}</pre>
+                        <pre>{JSON.stringify(data.matchData, null, 2)}</pre>
                     </div>
                 )}
             </div>
         </>
     );
-}
-
-async function fetchMatchHistory(
-    summonerId: string,
-    API_KEY: string,
-    setAccountData: (data: any) => void,
-    setMatchIdData: (data: any) => void,
-    setMatchData: (data: any) => void
-) {
-    const APIFetchAccount = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/${summonerId}?api_key=${API_KEY}`;
-    
-    try {
-        const response = await fetch(APIFetchAccount);
-        if (response.ok) {
-            const accData = await response.json();
-            const puuid = accData.puuid;
-            setAccountData(accData);
-            
-            await fetchMatchIds(puuid, API_KEY, setMatchIdData, setMatchData);
-            // Fetch match IDs and match data for the selected summoner
-            // using the PUUID from accData
-        } else {
-            console.error("API response error:", response.statusText);
-        }
-    } catch (error) {
-        console.error("Fetch error:", error);
-    }
-}
-
-// Fetch match IDs for a summoner based on their PUUID
-async function fetchMatchIds(
-    puuid: string, 
-    API_KEY: string, 
-    setMatchIdData: (data: any) => void,
-    setMatchData: (data: any) => void
-) {
-    // Validate the inputs
-    if (!puuid || !API_KEY) {
-        console.error("Invalid inputs: PUUID or API_KEY is missing.");
-        return;
-    }
-    
-    const APIFetchMatchId = `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=${API_KEY}`;
-
-    try {
-        const response = await fetch(APIFetchMatchId);
-        
-        if (response.ok) {
-            const matchIds = await response.json();
-            // Pass the fetched data to the setter function
-            setMatchIdData(matchIds);
-            const matchOne = matchIds[0];
-
-            await fetchMatchData(matchOne, API_KEY, setMatchData)
-        } else {
-            // Log the error message if response is not okay
-            console.error("API response error:", response.status, response.statusText);
-        }
-    } catch (error) {
-        // Handle fetch error
-        console.error("Fetch error:", error);
-    }
-}
-
-// Fetch match data for a summoners match ids
-
-async function fetchMatchData(
-    matchOne: number, 
-    API_KEY: string, 
-    setMatchData: (data: any) => void
-) {
-    // Validate the inputs
-    if (!matchOne || !API_KEY) {
-        console.error("Invalid inputs: PUUID or API_KEY is missing.");
-        return;
-    }
-
-    const FetchMatchData = `https://americas.api.riotgames.com/lol/match/v5/matches/${matchOne}?api_key=${API_KEY}`;
-
-    const response = await fetch(FetchMatchData);
-
-    try{
-        if(response.ok){
-            const data = await response.json();
-
-            setMatchData(data);
-
-        }
-        
-    } catch(error) {
-        // Handle fetch error
-        console.error("Fetch error:", error);
-    }
-
 }
